85ef22d393e0e3a4bb5e04e5b8ed9bff228910b8
luciiiferr <67103569+luciiiferr@users.noreply.github.com>
2020-08-02 12:09:47 +0530

    Update download.php

--- download.php
+++ download.php
@@ -1,23 +1,31 @@
 <?php
 
 function downloadChunk($dataList,$fileName){
+
 $files = file_get_contents($dataList);
 preg_match_all('#\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#iS', $files, $match);
 $ts = $match[0];
+
 foreach ($ts as $key){
+
     header('Content-Description: File Transfer');
     header('Content-Type: audio/mp3'); 
     header('Content-Transfer-Encoding: Binary');
     header('Content-Disposition: attachment; filename="'.$fileName.'.mp3"');
+
 $handle=fopen($key, 'rb');
 while (!feof($handle))
 {
   echo fread($handle, 8192);
     flush();
 }
-fclose($handle);
+
+   fclose($handle);
+
 }
-echo "Download Started";
+
+   echo "Download Started";
+
 }
 
 ?>
