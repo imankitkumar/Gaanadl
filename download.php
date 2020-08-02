87f48e30f77a030f370d6cf08e1af57f10e36b55
luciiiferr <67103569+luciiiferr@users.noreply.github.com>
2020-08-02 18:07:07 +0530

    Update download.php

--- download.php
+++ download.php
@@ -9,7 +9,7 @@ $ts = $match[0];
 foreach ($ts as $key){
 
     header('Content-Description: File Transfer');
-    header('Content-Type: video/MP2T'); 
+    header('Content-Type: video/mp4'); 
     header('Content-Transfer-Encoding: Binary');
     header('Content-Disposition: attachment; filename="'.$fileName.'.mp3"');
 
