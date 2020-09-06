6c1c712ffe7ca00fc9f099bf26eafe4fdfcef89b
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-09-06 16:47:02 +0530

    Update download.php

--- download.php
+++ download.php
@@ -3,21 +3,31 @@
 function downloadChunk($dataList,$fileName){
 
 $files = file_get_contents($dataList);
+
 preg_match_all('#\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#iS', $files, $match);
+
 $ts = $match[0];
 
 foreach ($ts as $key){
 
     header('Content-Description: File Transfer');
+
     header('Content-Type: octet/stream'); 
+
     header('Content-Transfer-Encoding: Binary');
+
     header('Content-Disposition: attachment; filename="'.$fileName.'.mp3"');
 
 $chunk=fopen($key, 'rb');
+
 while (!feof($chunk))
+
 {
+
   echo fread($chunk, 8192);
+
     flush();
+
 }
 
    fclose($chunk);
@@ -27,5 +37,6 @@ while (!feof($chunk))
    echo "Download Started";
 
 }
+
 
 ?>
