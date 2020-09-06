f3a3d03914eb0d42db4f3dcd38e05054aa5e4a44
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-09-06 16:50:39 +0530

    Update main.php

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ include('decrypt_url.php');
 include('download.php');
 
 $title = $_REQUEST['song'];
+
 $name = $_REQUEST['name'];
 
 if(isset($title,$name)){
@@ -12,31 +13,49 @@ if(isset($title,$name)){
 $header = array('user-agent: Mozilla/5.0 (Linux; Android 10;TXY567) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/8399.0.9993.96 Mobile Safari/599.36');
 
     $ch = curl_init();
+
     curl_setopt($ch, CURLOPT_URL, "https://gaana.com/apiv2?seokey=$name&type=songdetails&isChrome=1");
+
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
     curl_setopt($ch, CURLOPT_HEADER, 0);
+
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+
     $rspns = curl_exec($ch);
+
     $data = json_decode($rspns, true);
+
     $cipher = $data['tracks'][0]['urls']['auto']['message'];
     
 if(isset($cipher)){
 
 $dec_url = decryptUrl($cipher);
+
    $list_ts = file_get_contents($dec_url);
+
        preg_match_all('#\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))#iS', $list_ts, $match);
+
          $ts = $match[0];
+
            $filelist = $ts[1];
+
              downloadChunk($filelist,$title);
+
 }
+
 else   {
 
     echo "Decryption Failed.."; 
+
 }
+
 }
+
   else {
 
     echo "Provide SEO Query";
+
 }
 
 ?>
