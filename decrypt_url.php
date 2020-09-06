cc516af95e94d019a617647b421e0b3232fd34b7
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-09-06 16:46:01 +0530

    Update decrypt_url.php

--- decrypt_url.php
+++ decrypt_url.php
@@ -1,16 +1,24 @@
 <?php
 
+
 function decryptUrl($enc_url){
 
 $ciphering = "AES-128-CBC"; 
+
 $iv_length = openssl_cipher_iv_length($ciphering); 
+
 $options = 0;
+
 $decryption_iv = utf8_encode('asd!@#!@#@!12312');
+
 $decryption_key = utf8_encode('g@1n!(f1#r.0$)&%');
-$decrypted_url=openssl_decrypt ($enc_url, $ciphering,  
+
+$decrypted_url = openssl_decrypt($enc_url, $ciphering,  
         $decryption_key, $options, $decryption_iv);   
       
            return $decrypted_url;    
        
         }
+
+
 ?>
