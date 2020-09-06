d63b71241d45264bbda6c53bc2a4c52201ef37cb
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-09-06 16:49:04 +0530

    Update search.php

--- search.php
+++ search.php
@@ -2,23 +2,22 @@
 
 $q = $_GET['q'];
 
-$qu = urlencode($q);
+$search_query = urlencode($q);
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET,POST');
-header('Access-Control-Allow-Headers: X-Requested-With');
+$file = file_get_contents('https://gsearch.gaana.com/gaanasearch-api/mobilesuggest/autosuggest-lite-vltr-ro?geoLocation=IN&query='.$search_query.'&content_filter=2&include=track&isRegSrch=0%20&webVersion=mix&rType=web&usrLang=All&isChrome=');
 
-$file = file_get_contents('https://gsearch.gaana.com/gaanasearch-api/mobilesuggest/autosuggest-lite-vltr-ro?geoLocation=IN&query='.$qu.'&content_filter=2&include=track&isRegSrch=0%20&webVersion=mix&rType=web&usrLang=All&isChrome=');
+$json = json_decode($file, true);
 
-$de = json_decode($file, true);
-$tracks = $de['gr'][0]['gd'];
+$tracks = $json['gr'][0]['gd'];
 
 $arr = array();
 
 foreach ($tracks as $key => $value){
 
 $title = $value['ti'];
+
 $thumb = $value['aw'];
+
 $id = $value['seo'];
 
 $data = array("Title"=>"$title", "Pic_URL"=>"$thumb", "Main_URL"=>"main.php?song=$title&name=$id");
